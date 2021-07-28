d31671f8d22f5b3ed2b7213c48ce7f6d9e2069d9
Lewis Capers <lewiscapersjr206@gmail.com>
2021-07-27 20:45:28 -0700

    added data

--- data/sql/employees.sql
+++ data/sql/employees.sql
@@ -24,4 +24,26 @@ CREATE TABLE employee (
 );
 
 USE employee_db;
-INSERT INTO department
+INSERT INTO department (name)
+VALUES ('Production'),
+       ('Marketing'),
+       ('Accounting'),
+       ('Research and Development');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (001, 'Team Lead', 200,000, 1),
+       (002, 'CEO', 10,000,000, 3),
+       (003, 'Sales', 35,000, 2),
+       (004, 'Manager', 233,444, 1),
+       (005, 'Media', 50,000, 4),
+       (006, 'Manager Assistant', 45,000);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (001, 'Gary', 'Sanderson', 1, NULL),
+       (002, 'Jason', 'Hudson', 2, NULL),
+       (003, 'Rondald', 'Daniels', 3,),
+       (004, 'Frank', 'Woods', 5, NULL),
+       (005, 'Vladimir', 'Makarov', 6),
+       (006, 'Victor', 'Reznov', 4, NULL),
+       (007, 'John', 'Price', 8,),
+       (008, 'Ghost', 'MWTWO');
